2db9935f21877b0a5a07cadc9f07dbdcdd12cb56
kitakuukatik <101400160+kitakuukatik@users.noreply.github.com>
2026-03-25 08:13:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ function resetCount() {
   count = 0;
   document.getElementById("count").textContent = count;
 }
+
+function addTodo() {
+  const input = document.getElementById("todoInput");
+  const text = input.value;
+
+  if (text === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = text;
+
+  document.getElementById("todoList").appendChild(li);
+
+  input.value = "";
+}
